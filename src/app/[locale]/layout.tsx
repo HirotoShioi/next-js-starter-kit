291843b2eb440e2acf16a104639c4025f0cb49ba
Hiroto Shioi <shioihigg@gmail.com>
2024-07-26 11:51:54 +0900

    i18n

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,26 +1,41 @@
-import { locales } from '@/lib/i18n';
-import { NextIntlClientProvider } from 'next-intl';
-import {notFound} from 'next/navigation';
+import { locales } from "@/lib/i18n";
+import { cn } from "@/lib/utils";
+import { NextIntlClientProvider } from "next-intl";
+import { Noto_Sans_JP } from "next/font/google";
+import { notFound } from "next/navigation";
 
-export default async function LocaleLayout({children, params: {locale}}: 
-    {children: React.ReactNode, params: {locale: string}}
-) {
+const notoSansJp = Noto_Sans_JP({
+  subsets: ["latin"],
+  variable: "--font-noto-sans-jp",
+});
+
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
   let messages;
   try {
     messages = (await import(`../../../locales/${locale}.json`)).default;
-    console.log(messages);
   } catch (error) {
     notFound();
   }
   return (
-    <html lang={locale}>
-      <body>
+    <html lang={locale} suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background antialiased",
+          notoSansJp.variable,
+        )}
+      >
         <NextIntlClientProvider messages={messages}>
-        {children}
+          {children}
         </NextIntlClientProvider>
-        </body>
+      </body>
     </html>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,9 @@
-import "./globals.css"
-import { Inter as FontSans, Noto_Sans_JP } from "next/font/google"
+import "./globals.css";
 
-import { cn } from "@/lib/utils"
-
-const notoSansJp = Noto_Sans_JP({
-  subsets: ["latin"],
-  variable: "--font-noto-sans-jp",
-})
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          notoSansJp.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  )
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
 }
