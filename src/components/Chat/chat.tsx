e839a0ab55680fc571be766f5a9377d8f29ae5be
Hiroto Shioi <shioihigg@gmail.com>
2024-08-01 09:56:16 +0900

    markdown

--- src/components/Chat/chat.tsx
+++ src/components/Chat/chat.tsx
@@ -1,28 +1,31 @@
 "use client";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { BrainCircuit, SendIcon, User } from "lucide-react";
+import { BrainCircuit, SendIcon } from "lucide-react";
 import { useChat } from "ai/react";
 import { fetchAuthSession } from "aws-amplify/auth";
+import Markdown from "react-markdown";
 
-function UserMessage({ children }: { children: React.ReactNode }) {
+function UserMessage({ content }: { content: string }) {
   return (
     <div className="flex items-start justify-end">
-      <div className="bg-muted px-4 py-2 max-w-[75%] rounded-3xl">
-        {children}
+      <div className="bg-muted px-4 py-3 max-w-[75%] rounded-3xl">
+        {content}
       </div>
     </div>
   );
 }
 
-function AiMessage({ children }: { children: React.ReactNode }) {
+function AiMessage({ content }: { content: string }) {
   return (
     <div className="flex items-start gap-1 justify-start">
       <div className="p-2 bg-slate-300 rounded-full">
         <BrainCircuit size={28} />
       </div>
-      <div className="rounded-lg p-2 max-w">{children}</div>
+      <div className="rounded-lg p-2 max-w">
+        <Markdown>{content}</Markdown>
+      </div>
     </div>
   );
 }
@@ -46,20 +49,20 @@ export default function Chat() {
       <div className="flex-1  h-[83vh] overflow-y-auto p-6 space-y-3">
         {messages.map((message, index) => {
           if (message.role === "user") {
-            return <UserMessage key={index}>{message.content}</UserMessage>;
+            return <UserMessage key={index} content={message.content} />;
           } else {
-            return <AiMessage key={index}>{message.content}</AiMessage>;
+            return <AiMessage key={index} content={message.content} />;
           }
         })}
       </div>
       <form
         onSubmit={onSubmit}
-        className="bg-muted px-6 py-4 flex items-center gap-2 fixed bottom-0 w-full max-w-2xl"
+        className="px-6 py-4 flex items-center gap-2 fixed bottom-0 w-full max-w-2xl"
       >
         <Input
           id="message"
           placeholder="Type your message..."
-          className="flex-1"
+          className="flex-1 bg-muted"
           value={input}
           autoComplete="off"
           onChange={handleInputChange}
